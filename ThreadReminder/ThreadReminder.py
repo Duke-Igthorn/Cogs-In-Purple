353eca73d9b4a7b84fbc7c9297d1946ec8f8abec
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-01-20 22:46:00 +0100

    Update ThreadReminder.py

--- ThreadReminder/ThreadReminder.py
+++ ThreadReminder/ThreadReminder.py
@@ -21,53 +21,53 @@ class ThreadReminder(commands.Cog):
 
     @commands.command()
     async def threm(self, ctx, action: str, channel: discord.TextChannel = None, thread: str = None, *keywords):
-    if action == "set":
-        if channel is None or thread is None or len(keywords) == 0:
-            await ctx.send("Please provide a channel, thread and at least one keyword.")
-            return
-        self.settings[channel.id] = {"channel": channel.id, "thread": thread, "keywords": keywords, "active": False}
-        await ctx.send("Thread reminder set up for channel {} with thread {} and keywords {}".format(channel.mention, thread, keywords))
-        with open('thread_reminder_settings.json', 'w') as f:
-            json.dump(self.settings, f)
-    elif action == "activate":
-        if channel is None or thread is None:
-            await ctx.send("Please provide a channel and a thread.")
-            return
-        if channel.id not in self.settings:
-            await ctx.send("This channel has not been set up yet.")
-            return
-        if thread != self.settings[channel.id]["thread"]:
-            await ctx.send("This thread was not set up for this channel.")
-            return
-        self.settings[channel.id]["active"] = True
-        await ctx.send("Thread reminder for thread {} in channel {} is now active.".format(thread, channel.mention))
-        with open('thread_reminder_settings.json', 'w') as f:
-            json.dump(self.settings, f)
-    elif action == "deactivate":
-        if channel is None or thread is None:
-            await ctx.send("Please provide a channel and a thread.")
-            return
-        if channel.id not in self.settings:
-            await ctx.send("This channel has not been set up yet.")
-            return
-        if thread != self.settings[channel.id]["thread"]:
-            await ctx.send("This thread was not set up for this channel.")
-            return
-        self.settings[channel.id]["active"] = False
-        await ctx.send("Thread reminder for thread {} in channel {} is now inactive.".format(thread, channel.mention))
-        with open('thread_reminder_settings.json', 'w') as f:
-            json.dump(self.settings, f)
-    elif action == "list":
-        if len(self.settings) == 0:
-            await ctx.send("No thread reminders have been set up yet.")
-            return
-        message = "Thread Reminders:\n"
-        for channel_id in self.settings:
-            channel = self.bot.get_channel(int(channel_id))
-            if channel is None:
-                continue
-            message += "- {}: thread {}, keywords {}, active: {}\n".format(channel.mention, self.settings[channel_id]["thread"], self.settings[channel_id]["keywords"], self.settings[channel_id]["active"])
-            await ctx.send(message)
+        if action == "set":
+            if channel is None or thread is None or len(keywords) == 0:
+                await ctx.send("Please provide a channel, thread and at least one keyword.")
+                return
+            self.settings[channel.id] = {"channel": channel.id, "thread": thread, "keywords": keywords, "active": False}
+            await ctx.send("Thread reminder set up for channel {} with thread {} and keywords {}".format(channel.mention, thread, keywords))
+            with open('thread_reminder_settings.json', 'w') as f:
+                json.dump(self.settings, f)
+        elif action == "activate":
+            if channel is None or thread is None:
+                await ctx.send("Please provide a channel and a thread.")
+                return
+            if channel.id not in self.settings:
+                await ctx.send("This channel has not been set up yet.")
+                return
+            if thread != self.settings[channel.id]["thread"]:
+                await ctx.send("This thread was not set up for this channel.")
+                return
+            self.settings[channel.id]["active"] = True
+            await ctx.send("Thread reminder for thread {} in channel {} is now active.".format(thread, channel.mention))
+            with open('thread_reminder_settings.json', 'w') as f:
+                json.dump(self.settings, f)
+        elif action == "deactivate":
+            if channel is None or thread is None:
+                await ctx.send("Please provide a channel and a thread.")
+                return
+            if channel.id not in self.settings:
+                await ctx.send("This channel has not been set up yet.")
+                return
+            if thread != self.settings[channel.id]["thread"]:
+                await ctx.send("This thread was not set up for this channel.")
+                return
+            self.settings[channel.id]["active"] = False
+            await ctx.send("Thread reminder for thread {} in channel {} is now inactive.".format(thread, channel.mention))
+            with open('thread_reminder_settings.json', 'w') as f:
+                json.dump(self.settings, f)
+        elif action == "list":
+            if len(self.settings) == 0:
+                await ctx.send("No thread reminders have been set up yet.")
+                return
+            message = "Thread Reminders:\n"
+            for channel_id in self.settings:
+                channel = self.bot.get_channel(int(channel_id))
+                if channel is None:
+                    continue
+                message += "- {}: thread {}, keywords {}, active: {}\n".format(channel.mention, self.settings[channel_id]["thread"], self.settings[channel_id]["keywords"], self.settings[channel_id]["active"])
+                await ctx.send(message)
 
 @commands.Cog.listener()
 async def on_message(self, message):
