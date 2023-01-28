f0c8baa93844ab797714bc7cd50e93814e1a8470
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-01-28 01:45:10 +0100

    Update MessageMover.py

--- MessageMover/MessageMover.py
+++ MessageMover/MessageMover.py
@@ -8,22 +8,34 @@ from discord import TextChannel
 class MessageMover(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    async def msgmvr(self, ctx, destination: discord.TextChannel, *message_ids: int):
+        
+    @commands.command(name="msgmvr")
+    async def _msgmvr(self, ctx, destination: discord.TextChannel, *message_ids: int):
+     if not message_ids:
+        await ctx.send("Please provide at least one message ID to move.")
+        return
+     if not destination:
+        await ctx.send("Please provide a valid destination channel.")
+        return
         messages = []
         for message_id in message_ids:
+        try:
             message = await ctx.channel.fetch_message(message_id)
             messages.append(message)
-        
+        except discord.errors.NotFound:
+            await ctx.send(f"The message with ID {message_id} could not be found in this channel.")
+            continue
+    
         messages.sort(key=lambda x: x.created_at)
-        
-        for message in messages:
-            await message.delete()
-            await destination.send(content=message.content,
-                                   author=message.author,
-                                   embed=message.embeds[0] if message.embeds else None,
-                                   file=message.attachments[0].url if message.attachments else None,
-                                   allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False))
+    
+    for message in messages:
+        await message.pin()
+        await message.unpin()
+        await message.delete()
+        await destination.send(content=message.content,
+                               embed=message.embeds[0] if message.embeds else None,
+                               file=message.attachments[0].url if message.attachments else None,
+                               allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False))
             
 
 def setup(bot):
