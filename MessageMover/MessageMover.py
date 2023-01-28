a05cbafdf04f1e675c3b6a9ffa6313b12ddd02d6
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-01-28 02:10:42 +0100

    Update MessageMover.py

--- MessageMover/MessageMover.py
+++ MessageMover/MessageMover.py
@@ -10,18 +10,17 @@ class MessageMover(commands.Cog):
     @commands.command()
     async def msgmvr(self, ctx, destination: discord.TextChannel, *message_ids: int):
         if not message_ids:
-            return await ctx.send("Please provide at least one message ID to move.")
-        
+            return await ctx.send("You must specify at least one message ID to move.")
         if not destination:
-            return await ctx.send("Please provide a destination channel to move the messages to.")
-        
+            return await ctx.send("You must specify a destination channel.")
+
         messages = []
         for message_id in message_ids:
             message = await ctx.channel.fetch_message(message_id)
             if message:
                 messages.append(message)
             else:
-                return await ctx.send(f"Could not find a message with the ID {message_id}. Please check that the ID is correct.")
+                return await ctx.send(f"Could not find message with ID {message_id}.")
         
         messages.sort(key=lambda x: x.created_at)
         
@@ -29,11 +28,15 @@ class MessageMover(commands.Cog):
             await message.pin()
             await message.unpin()
             await message.delete()
-            await destination.send(content=message.content,
+            sent_message = await destination.send(content=message.content,
                                    embed=message.embeds[0] if message.embeds else None,
-                                   files=[discord.File(await message.attachments[0].to_file(), filename=message.attachments[0].filename) ] if message.attachments else None,
-                                   allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False),
-                                   author=message.author)
+                                   file=message.attachments[0].url if message.attachments else None,
+                                   allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False))
+            await sent_message.edit(content=message.content,
+                                    embed=message.embeds[0] if message.embeds else None,
+                                    file=message.attachments[0].url if message.attachments else None,
+                                    allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False),
+                                    author=message.author)
 
 def setup(bot):
     bot.add_cog(MessageMover(bot))
