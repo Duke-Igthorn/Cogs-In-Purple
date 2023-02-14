2636f3a130cc3f44c8410941c0b5466f9c017499
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-02-14 23:24:17 +0100

    Update MessageMover.py

--- MessageMover/MessageMover.py
+++ MessageMover/MessageMover.py
@@ -6,7 +6,7 @@ class MessageMover(commands.Cog):
         self.bot = bot
 
     @commands.command(name="msgmvr")
-    async def move_messages(self, ctx):
+    async def move_messages(self, ctx, dest_channel: discord.TextChannel):
         await ctx.send("Please provide the message IDs or ranges to move (separated by commas):")
         msg_ids_input = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
         msg_ids = []
@@ -17,20 +17,12 @@ class MessageMover(commands.Cog):
             else:
                 msg_ids.append(int(msg_id))
 
-        await ctx.send("Please provide the destination channel:")
-        dest_channel_input = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
-        dest_channel = discord.utils.get(ctx.guild.channels, name=dest_channel_input.content)
-
-        if dest_channel is None or not isinstance(dest_channel, discord.TextChannel):
-            await ctx.send("Invalid channel name. Please try again.")
-            return
-
         for msg_id in msg_ids:
             message = await ctx.channel.fetch_message(msg_id)
             if message is None:
                 await ctx.send(f"Message with ID {msg_id} not found.")
                 continue
-
+            
             embed = discord.Embed(
                 description=message.content,
                 timestamp=message.created_at
