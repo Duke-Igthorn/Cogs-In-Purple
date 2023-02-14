cf2aa7240868ee4f85880a7887eeaf37b2bacd34
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-02-14 23:12:15 +0100

    Update MessageMover.py

--- MessageMover/MessageMover.py
+++ MessageMover/MessageMover.py
@@ -1,43 +1,52 @@
-# Broken Cog, needs fixing!
-import asyncio
-import datetime
-from redbot.core import commands
 import discord
+from discord.ext import commands
 
 class MessageMover(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def msgmvr(self, ctx, destination: discord.TextChannel, *message_ids: int):
-        if not message_ids:
-            return await ctx.send("You must specify at least one message ID to move.")
-        if not destination:
-            return await ctx.send("You must specify a destination channel.")
-
-        messages = []
-        for message_id in message_ids:
-            message = await ctx.channel.fetch_message(message_id)
-            if message:
-                messages.append(message)
+    @commands.command(name="msgmvr")
+    async def move_messages(self, ctx):
+        await ctx.send("Please provide the message IDs or ranges to move (separated by commas):")
+        msg_ids_input = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
+        msg_ids = []
+        for msg_id in msg_ids_input.content.split(','):
+            if '-' in msg_id:
+                range_start, range_end = map(int, msg_id.split('-'))
+                msg_ids += [i for i in range(range_start, range_end+1)]
             else:
-                return await ctx.send(f"Could not find message with ID {message_id}.")
+                msg_ids.append(int(msg_id))
+
+        await ctx.send("Please provide the destination channel:")
+        dest_channel_input = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and m.channel == ctx.channel)
+        dest_channel = discord.utils.get(ctx.guild.channels, name=dest_channel_input.content)
         
-        messages.sort(key=lambda x: x.created_at)
-        
-        for message in messages:
-            await message.pin()
-            await message.unpin()
+        if dest_channel is None:
+            await ctx.send("Invalid channel name. Please try again.")
+            return
+
+        for msg_id in msg_ids:
+            message = await ctx.channel.fetch_message(msg_id)
+            if message is None:
+                await ctx.send(f"Message with ID {msg_id} not found.")
+                continue
+            
+            embed = discord.Embed(
+                description=message.content,
+                timestamp=message.created_at
+            )
+            embed.set_author(
+                name=message.author.name,
+                icon_url=message.author.avatar_url
+            )
+
+            if len(message.attachments) > 0:
+                embed.set_image(url=message.attachments[0].url)
+
+            await dest_channel.send(embed=embed)
             await message.delete()
-            sent_message = await destination.send(content=message.content,
-                                   embed=message.embeds[0] if message.embeds else None,
-                                   file=message.attachments[0].url if message.attachments else None,
-                                   allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False))
-            await sent_message.edit(content=message.content,
-                                    embed=message.embeds[0] if message.embeds else None,
-                                    file=message.attachments[0].url if message.attachments else None,
-                                    allowed_mentions=discord.AllowedMentions(users=False, roles=False, everyone=False),
-                                    author=message.author)
+
+        await ctx.send("Messages moved successfully.")
 
 def setup(bot):
     bot.add_cog(MessageMover(bot))
