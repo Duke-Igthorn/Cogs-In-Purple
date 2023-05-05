c35c2ad2d9c536851b57ae4ac556751beefa3f2f
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-05-05 08:02:06 +0200

    Update CliveCog.py

--- CliveCog/CliveCog.py
+++ CliveCog/CliveCog.py
@@ -12,12 +12,16 @@ class CliveCog(commands.Cog):
         if ctx.channel.name.lower() != "live":
             await ctx.send("This command can only be used in the `live` channel.")
             return
+
         # Get the first message in the channel
-        first_message = await ctx.channel.history(limit=1, oldest_first=True).flatten()
-        self.memorized_message = first_message[0].content if first_message else None
+        async for message in ctx.channel.history(limit=1, oldest_first=True):
+            first_message = message
+            break
+        self.memorized_message = first_message.content if first_message else None
         if self.memorized_message is None:
             await ctx.send("No message to remember.")
             return
+
         # Delete all messages in the channel
         await ctx.channel.purge(limit=None)
 
@@ -42,5 +46,5 @@ class CliveCog(commands.Cog):
         await ctx.send(f"{self.memorized_message}")
 
 
-def setup(bot):
-    bot.add_cog(CliveCog(bot))        
+async def setup(bot):
+    await bot.add_cog(CliveCog(bot))
