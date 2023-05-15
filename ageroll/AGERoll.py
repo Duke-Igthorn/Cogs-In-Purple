212bc2026c9652ff8d8fa5c8fd488c97386ece2a
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-05-15 22:56:20 +0200

    Update AGERoll.py

--- ageroll/AGERoll.py
+++ ageroll/AGERoll.py
@@ -2,13 +2,20 @@ import random
 from redbot.core import commands
 
 class AGERoll(commands.Cog):
+    @staticmethod
+    def roll_dice():
+        return random.randint(1, 6)
+        
     @commands.command()
     async def ageroll(self, ctx, target_number: int = 11, modifier: int = 0):
-        def roll_dice():
-            return random.randint(1, 6)
-        
-        dice = [roll_dice(), roll_dice(), roll_dice()]
+        if not isinstance(target_number, int) or not isinstance(modifier, int):
+            await ctx.send("Both target number and modifier need to be integers.")
+            return
+
+        dice = [self.roll_dice(), self.roll_dice(), self.roll_dice()]
         total = sum(dice) + modifier
+        stunt_points = None
+
         if total >= target_number:
             if dice[0] == dice[1] or dice[0] == dice[2] or dice[1] == dice[2]:
                 stunt_points = dice[0]
@@ -24,9 +31,10 @@ class AGERoll(commands.Cog):
                     await ctx.send("Botch!")
                 else:
                     await ctx.send("Failure!")
+        
         await ctx.send(f"Stunt Die: {dice[0]}\nDie 2    : {dice[1]}\nDie 3    : {dice[2]}\nTotal: {total}")
-        if "stunt_points" in locals():
+        if stunt_points is not None:
             await ctx.send(f"Stunt Points: {stunt_points}")
 
 def setup(bot):
-    bot.add_cog(AGERoll())
+    bot.add_cog(AGERoll(bot))
