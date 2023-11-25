31edc0af16407ba560d4b7bf5f01713785c03cd1
The Duke In Purple <Herzog.Igzorn@gmail.com>
2023-11-26 00:20:03 +0100

    MessageMover.py

--- MessageMover/MessageMover.py
+++ MessageMover/MessageMover.py
@@ -1,102 +1,69 @@
 import discord
-from redbot.core import commands
-from typing import List, Tuple
 import asyncio
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
 
 class MessageMover(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="msgmvr")
-    async def move_messages(self, ctx, dest_channel: discord.TextChannel, *message_ids: str):
-        ranges, singles, invalid = self.parse_message_ids(message_ids)
+    MAX_BATCH_SIZE = 10  # Maximum number of messages to move in one batch
+    MAX_EMBED_DESCRIPTION_SIZE = 2048  # Maximum characters for an embed description
 
-        messages = []
-        async for message in ctx.channel.history():
-            for start_id, end_id in ranges:
-                if start_id <= message.id <= end_id:
-                    messages.insert(0, message)
-                    break
-            if message.id in singles:
-                messages.insert(0, message)
-
-        if len(messages) == 0:
-            await ctx.send("No messages found in the specified range.")
+    @cog_ext.cog_slash(
+        name="move",
+        description="Move messages to another channel",
+        options=[
+            create_option(
+                name="channel",
+                description="The channel to move messages to",
+                option_type=7,  # Channel type
+                required=True
+            ),
+            create_option(
+                name="message_ids",
+                description="IDs of the messages to move",
+                option_type=3,  # String type, as slash commands do not support variadic arguments
+                required=True
+            ),
+        ]
+    )
+    async def _move_messages(self, ctx: SlashContext, channel: discord.TextChannel, message_ids: str):
+        # Parse the message IDs from the string
+        try:
+            message_ids = [int(id.strip()) for id in message_ids.split(",")]
+        except ValueError:
+            await ctx.send("Error: Invalid message IDs. Please enter a comma-separated list of IDs.")
             return
 
-        invalid_messages = []
-        for message in messages:
-            embed = discord.Embed(
-                description=message.content,
-                timestamp=message.created_at
-            )
-            embed.set_author(
-                name=message.author.name,
-                icon_url=message.author.avatar_url
-            )
+        if len(message_ids) > self.MAX_BATCH_SIZE:
+            await ctx.send(f'🚨 This command is limited to moving {self.MAX_BATCH_SIZE} messages at a time to avoid rate limiting issues. Please split your request into smaller batches.')
+            return
 
-            if len(message.attachments) > 0:
-                embed.set_image(url=message.attachments[0].url)
-
-            try:
-                new_message = await dest_channel.send(embed=embed)
-                await message.delete()
-            except discord.errors.NotFound:
-                invalid_messages.append(str(message.id))
-
-        if len(invalid_messages) > 0:
-            invalid_message = f"The following messages could not be moved: {', '.join(invalid_messages)}"
-            await ctx.send(invalid_message)
-
-        confirmation_message = await ctx.send("Messages moved successfully. Remove the command messages?")
-        await confirmation_message.add_reaction("✅")
-        await confirmation_message.add_reaction("❌")
-
-        def check(reaction, user):
-            return user == ctx.author and reaction.message == confirmation_message
-
-        try:
-            reaction, user = await self.bot.wait_for('reaction_add', timeout=20.0, check=check)
-            if str(reaction.emoji) == "✅":
-                messages_to_delete = [confirmation_message]
-                async for message in ctx.channel.history(after=ctx.message):
-                    if message.author == self.bot.user and message.id not in messages_to_delete:
-                        messages_to_delete.append(message)
-                for message in messages_to_delete:
-                    try:
-                        await message.delete()
-                    except discord.errors.NotFound:
-                        pass
-                await ctx.message.delete()
-        except asyncio.TimeoutError:
-            pass
-
-    def parse_message_ids(self, message_ids: List[str]) -> Tuple[List[Tuple[int, int]], List[int], List[str]]:
-        ranges = []
-        singles = []
-        invalid = []
         for message_id in message_ids:
-            if "-" in message_id:
-                try:
-                    start_id, end_id = message_id.split("-")
-                    start_id = int(start_id)
-                    end_id = int(end_id)
-                    if start_id > end_id:
-                        start_id, end_id = end_id, start_id
-                    ranges.append((start_id, end_id))
-                except ValueError:
-                    invalid.append(message_id)
-            else:
-                try:
-                    singles.append(int(message_id))
-                except ValueError:
-                    invalid.append(message_id)
+            try:
+                message = await ctx.channel.fetch_message(message_id)
+                content = message.content
+                while content:
+                    embed_content = content[:self.MAX_EMBED_DESCRIPTION_SIZE]
+                    content = content[self.MAX_EMBED_DESCRIPTION_SIZE:]
 
-        if len(invalid) > 0:
-            invalid_message = f"The following message IDs are invalid: {', '.join(invalid)}"
-            ctx.send(invalid_message)
+                    embed = discord.Embed(description=embed_content, color=0x3498db)
+                    embed.set_author(name=message.author.display_name, icon_url=message.author.avatar_url)
+                    await channel.send(embed=embed)
 
-        return ranges, singles, invalid
+                await message.delete()
+            except discord.NotFound:
+                await ctx.send(f'Message with ID {message_id} not found.')
+            except discord.Forbidden:
+                await ctx.send('I do not have permission to perform this action.')
+            except discord.HTTPException as e:
+                await ctx.send(f'Failed to move message: {e}')
+
+        completion_message = await channel.send('✅ Move complete. You can now resume posting messages.')
+        await asyncio.sleep(20)
+        await completion_message.delete()
 
 def setup(bot):
     bot.add_cog(MessageMover(bot))
